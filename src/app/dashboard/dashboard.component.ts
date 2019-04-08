9c0b2276906cf34baef69780437dad597245bdbb
Martin Vestberg <martinvestberg@Martins-MacBook-Pro.local>
2019-04-08 10:11:17 +0200

    Added component

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ns-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css'],
+  moduleId: module.id,
+})
+export class DashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
