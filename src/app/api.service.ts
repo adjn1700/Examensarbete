c22ed91fbfd59bdf34a072b1af5f1659759d1cbe
Adam Jönehag <AdamJonehag@Adams-MacBook-Pro.local>
2019-04-09 14:50:27 +0200

    api

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -11,8 +11,32 @@ export class ApiService {
 
     constructor(private http: HttpClient) { }
 
+    //Get data api
     getData(){
+        let headers = this.createRequestHeader();
+        return this.http.get(this.apiUrl, { headers: headers });
+    }
+    private createRequestHeader() {
+        // set headers here e.g.
+        let headers = new HttpHeaders({
+            "AuthKey": "my-key",
+            "AuthToken": "my-token",
+            "Content-Type": "application/json",
+         });
 
+        return headers;
     }
 
+    //Post data api
+    postData(data: any) {
+        let options = this.createRequestOptions();
+        return this.http.post(this.apiUrl, { data }, { headers: options });
+    }
+
+    private createRequestOptions() {
+        let headers = new HttpHeaders({
+            "Content-Type": "application/json"
+        });
+        return headers;
+    }
 }
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -8,18 +8,55 @@ import { confirm, ConfirmOptions } from "tns-core-modules/ui/dialogs";
   selector: 'ns-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css'],
-  moduleId: module.id,
+  moduleId: module.id
 })
 export class DashboardComponent implements OnInit {
 
-  constructor(myapiService: ApiService, private router: RouterExtensions) { }
+    public host: string;
+    public userAgent: string;
+    public origin: string;
+    public url: string;
+
+  constructor(private myapiService: ApiService, private router: RouterExtensions) { }
 
   ngOnInit() {
-
+    //console.log(this.extractData());
 }
+    //Api Get
+    extractData() {
+        this.myapiService.getData()
+            .subscribe((result) => {
+                this.onGetDataSuccess(result);
+            }, (error) => {
+                console.log(error);
+            });
+    }
+
+    private onGetDataSuccess(res) {
+        this.host = res.headers.Host;
+        this.userAgent = res.headers["User-Agent"];
+        this.origin = res.origin;
+        this.url = res.url;
+    }
+    //Api Post
+    public user: string;
+    public pass: string;
+    public message: string = "";
 
 
+    public submit() {
+        this.makePostRequest();
+    }
 
+    private makePostRequest() {
+        this.myapiService
+            .postData({ username: this.user, password: this.pass })
+            .subscribe(res => {
+                this.message = (<any>res).json.data.username;
+            });
+    }
+
+    //Alert
   cancelAlert(){
     let options: ConfirmOptions = {
         title: "Avsluta",
@@ -34,6 +71,7 @@ export class DashboardComponent implements OnInit {
     });
   }
 
+  //Navigering
   backToStart(){
     this.router.navigate(['/start-screen'], {
         clearHistory: true,
@@ -44,7 +82,4 @@ export class DashboardComponent implements OnInit {
         }
     } );
   }
-
-
-
 }
