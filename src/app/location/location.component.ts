9578840a0a07f22a08aba148a9329030e1271d8f
Martin Vestberg <martinvestberg@Martins-MacBook-Pro.local>
2019-04-10 10:47:58 +0200

    Removed service

--- src/app/location/location.component.ts
+++ src/app/location/location.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { LocationService } from './location.service';
+import { Component, OnInit, NgZone } from '@angular/core';
+import * as Geolocation from "nativescript-geolocation";
 
 
 @Component({
@@ -12,13 +12,19 @@ export class LocationComponent implements OnInit {
 
     public latitude: number;
     public longitude: number;
+    private watchId: number;
 
-    public constructor(private locationService : LocationService ) {
+    public constructor(private zone: NgZone) {
+        this.latitude = 0;
+        this.longitude = 0;
+    }
+
+    ngOnInit(){
+        this.startWatchingLocation();
     }
 
     public updateLocation() {
-        this.locationService.getDeviceLocation()
-        .then(result => {
+        this.getDeviceLocation().then(result => {
             this.latitude = result.latitude;
             this.longitude = result.longitude;
         }, error => {
@@ -26,8 +32,37 @@ export class LocationComponent implements OnInit {
         });
     }
 
-    ngOnInit(){
-        this.updateLocation();
+    public getDeviceLocation(): Promise<any> {
+        return new Promise((resolve, reject) => {
+            Geolocation.enableLocationRequest().then(() => {
+                Geolocation.getCurrentLocation({timeout: 10000}).then(location => {
+                    resolve(location);
+                    console.log(location);
+                }).catch(error => {
+                    reject(error);
+                });
+            });
+        });
+    }
+
+    public startWatchingLocation() {
+        this.watchId = Geolocation.watchLocation(location => {
+            if(location) {
+                this.zone.run(() => {
+                    this.latitude = location.latitude;
+                    this.longitude = location.longitude;
+                });
+            }
+        }, error => {
+            //console.dump(error);
+        }, { updateDistance: 1, minimumUpdateTime: 1000 });
+    }
+
+    public stopWatchingLocation() {
+        if(this.watchId) {
+            Geolocation.clearWatch(this.watchId);
+            this.watchId = null;
+        }
     }
 
 }
